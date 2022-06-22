5cc2fe0e624458ef3a97b1e0723115f31029f109
alfgiron30 <102273916+alfgiron30@users.noreply.github.com>
2022-06-21 23:32:36 -0500

    Add files via upload

---
+++ precache-manifest.e21b335405644aa2301e1b3c1de3cb7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d479062fe49d819587cf5ef52e5c268",
+    "url": "/dsos-payment-frontend/index.html"
+  },
+  {
+    "revision": "a94c241cfc6ea4b92c53",
+    "url": "/dsos-payment-frontend/static/css/main.967f9c85.chunk.css"
+  },
+  {
+    "revision": "51fae0963a53172ee645",
+    "url": "/dsos-payment-frontend/static/js/2.24b9f166.chunk.js"
+  },
+  {
+    "revision": "a94c241cfc6ea4b92c53",
+    "url": "/dsos-payment-frontend/static/js/main.898d9069.chunk.js"
+  },
+  {
+    "revision": "84db57c06b413a5f39c3",
+    "url": "/dsos-payment-frontend/static/js/runtime~main.62e180ea.js"
+  }
+]);
